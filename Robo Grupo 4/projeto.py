abd157d6b1392a54085f1f1d6fa09f9422990069
Erik Gimenez <Erikgs2006@gmail.com>
2025-05-19 14:34:52 -0300

    Add files via upload

--- Robo Grupo 4/projeto.py
+++ Robo Grupo 4/projeto.py
@@ -15,93 +15,118 @@ import motor
 # F = Nada
 
 PORTA_SENSOR_ESQ = port.A
-SENSOR_DIR = 0
 SENSOR_ESQ = 0
+SENSOR_DIR = 0
 PORTA_SENSOR_DIR = port.B
 MOTOR_RODA_ESQ = port.C
 MOTOR_RODA_DIR = port.D
 MOTOR_CENTRAL = port.E
 
-LIMIAR = 60
-#tentar adicionar HISTERESE
+linha_esq = False
+linha_dir = False
 
-def estouNaLinha(valor_sensor):
-    if valor_sensor >= LIMIAR:
-        print("Linha")
-        sensor_linha = True
+LIMIAR = 50
+HISTERESE = 5
+
+SENSOR_ESQ = color_sensor.reflection(PORTA_SENSOR_ESQ)
+if linha_esq:
+    if SENSOR_ESQ > (LIMIAR + HISTERESE) :
+        print("Linha", SENSOR_ESQ)
+        linha_esq = True
+else:
+    if  SENSOR_ESQ < (LIMIAR - HISTERESE) :
+        print("Não linha", SENSOR_ESQ)
+        linha_esq = False
+
+SENSOR_DIR = color_sensor.reflection(PORTA_SENSOR_DIR)
+if linha_dir:
+    if SENSOR_DIR > (LIMIAR + HISTERESE) :
+        print("Linha", SENSOR_DIR)
+        linha_dir = True
     else:
-        print("Não linha")
-        sensor_linha = False
-    return sensor_linha
+        if SENSOR_DIR < (LIMIAR - HISTERESE) :
+            print("Não linha", SENSOR_DIR)
+            linha_dir = False
 
-def tras(velocidade):
-    motor.run(MOTOR_RODA_DIR, -velocidade)
-    motor.run(MOTOR_RODA_ESQ, velocidade)
+# def estouNaLinha(valor_sensor):
+#    if valor_sensor > (LIMIAR + HISTERESE):
+#        print("Linha", valor_sensor)
+#        sensor_linha = True
+#    elif valor_sensor < (LIMIAR - HISTERESE):
+#        print("Não linha", valor_sensor)
+#        sensor_linha = False
+#    else:
+#        sensor_linha = False
+#    return sensor_linha
 
-def frente(velocidade):
-    motor.run(MOTOR_RODA_DIR, velocidade)
-    motor.run(MOTOR_RODA_ESQ, -velocidade)
+    def tras(velocidade):
+        motor.run(MOTOR_RODA_DIR, -velocidade)
+        motor.run(MOTOR_RODA_ESQ, velocidade)
 
-def parar():
-    velocidade = 0
-    motor.run(MOTOR_RODA_DIR, -velocidade)
-    motor.run(MOTOR_RODA_ESQ, velocidade)
+    def frente(velocidade):
+        motor.run(MOTOR_RODA_DIR, velocidade)
+        motor.run(MOTOR_RODA_ESQ, -velocidade)
 
-def girarEsquerda(velocidade):
-    motor.run(MOTOR_RODA_DIR, velocidade)
-    motor.run(MOTOR_RODA_ESQ, 0)
+    def parar():
+        velocidade = 0
+        motor.run(MOTOR_RODA_DIR, -velocidade)
+        motor.run(MOTOR_RODA_ESQ, velocidade)
 
-def girarDireita(velocidade):
-    motor.run(MOTOR_RODA_DIR, 0)
-    motor.run(MOTOR_RODA_ESQ, -velocidade)
+    def girarEsquerda(velocidade):
+        motor.run(MOTOR_RODA_DIR, velocidade)
+        motor.run(MOTOR_RODA_ESQ, 0)
+
+    def girarDireita(velocidade):
+        motor.run(MOTOR_RODA_DIR, 0)
+        motor.run(MOTOR_RODA_ESQ, -velocidade)
 
 
-async def main():
-    await light_matrix.write("!")
-    velocidade = 100
-    while True:
-        SENSOR_DIR = color_sensor.reflection(PORTA_SENSOR_DIR)
-        SENSOR_ESQ = color_sensor.reflection(PORTA_SENSOR_ESQ)
-        linhaEsq = estouNaLinha(SENSOR_ESQ)
-        linhaDir = estouNaLinha(SENSOR_DIR)
-        # sensor mais proximo 
-        if (not linhaDir) and (not linhaEsq):
-            frente(velocidade)
-        elif linhaDir and (not linhaEsq):
-            girarDireita(velocidade)
-        elif (not linhaDir) and linhaEsq:
-            girarEsquerda(velocidade)
-        else:
-            parar()
-        await runloop.sleep_ms(10)
+    async def main():
+        await light_matrix.write("!")
+        velocidade = 100
+        while True:
+            # SENSOR_DIR = color_sensor.reflection(PORTA_SENSOR_DIR)
+            # SENSOR_ESQ = color_sensor.reflection(PORTA_SENSOR_ESQ)
+            # linhaEsq = estouNaLinha(SENSOR_ESQ)
+            # linhaDir = estouNaLinha(SENSOR_DIR)
+            # sensor mais proximo
+            if (not linha_dir) and (not linha_esq):
+                frente(velocidade)
+            elif linha_dir and (not linha_esq):
+                girarDireita(velocidade)
+            elif (not linha_dir) and linha_esq:
+                girarEsquerda(velocidade)
+            else:
+                parar()
+            await runloop.sleep_ms(10)
 
-        # ORIGINAL
-        # if (not linhaDir) and (not linhaEsq):
-        #     frente(velocidade)
-        # if linhaDir and (not linhaEsq):
-        #     girarDireita(velocidade)
-        # if (not linhaDir) and linhaEsq:
-        #     girarEsquerda(velocidade)
-        # if linhaDir and linhaEsq:
-        #     parar()
+            # ORIGINAL
+            # if (not linhaDir) and (not linhaEsq):
+            #    frente(velocidade)
+            # if linhaDir and (not linhaEsq):
+            #    girarDireita(velocidade)
+            # if (not linhaDir) and linhaEsq:
+            #    girarEsquerda(velocidade)
+            # if linhaDir and linhaEsq:
+            #    parar()
 
-        # VERSÃO MAIS ANTIGA
-        # if color_sensor.reflection(PORTA_SENSOR_ESQ) > 60 & color_sensor.reflection(PORTA_SENSOR_DIR) > 65:
-        #     parar()
-        # if color_sensor.reflection(PORTA_SENSOR_ESQ) < 40 & color_sensor.reflection(PORTA_SENSOR_DIR) < 35:
-        #     frente(100)
-        # if color_sensor.reflection(PORTA_SENSOR_ESQ) > 60:
-        #     girarEsquerda(100)
-        # if color_sensor.reflection(PORTA_SENSOR_ESQ) > 65:
-        #     girarDireita(100)
+            # VERSÃO MAIS ANTIGA
+            # if color_sensor.reflection(PORTA_SENSOR_ESQ) > 60 & color_sensor.reflection(PORTA_SENSOR_DIR) > 65:
+            #    parar()
+            # if color_sensor.reflection(PORTA_SENSOR_ESQ) < 40 & color_sensor.reflection(PORTA_SENSOR_DIR) < 35:
+            #    frente(100)
+            # if color_sensor.reflection(PORTA_SENSOR_ESQ) > 60:
+            #    girarEsquerda(100)
+            # if color_sensor.reflection(PORTA_SENSOR_ESQ) > 65:
+            #    girarDireita(100)
 
-        # ADAPTAÇÃO DA VERSÃO ANTIGA USANDO AS COMPARAÇÕES MAIS DIRETAS
-        # if (SENSOR_ESQ > 60) and (SENSOR_DIR > 65):
-        #     parar()
-        # elif (SENSOR_ESQ < 60) and (SENSOR_DIR < 65):
-        #     frente(velocidade)
-        # elif linhaEsq:
-        #     girarEsquerda(velocidade)
-        # else:
-        #     girarDireita(velocidade)
-runloop.run(main())
+            # ADAPTAÇÃO DA VERSÃO ANTIGA USANDO AS COMPARAÇÕES MAIS DIRETAS
+            # if (SENSOR_ESQ > 60) and (SENSOR_DIR > 65):
+            #    parar()
+            # elif (SENSOR_ESQ < 60) and (SENSOR_DIR < 65):
+            #    frente(velocidade)
+            # elif linhaEsq:
+            #    girarEsquerda(velocidade)
+            # else:
+            #    girarDireita(velocidade)
+    runloop.run(main())
