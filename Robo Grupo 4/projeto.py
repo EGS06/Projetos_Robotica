5b77f2fb5a1ff10ce8b7bf76f8034838050af219
Erik Gimenez <Erikgs2006@gmail.com>
2025-05-26 16:54:17 -0300

    Add files via upload

--- Robo Grupo 4/projeto.py
+++ Robo Grupo 4/projeto.py
@@ -6,29 +6,40 @@ import time
 import color
 import color_sensor
 import motor
+# testar controlar velocidade de motores
+# testar logica pois se uma das rodas ja estiver parada quandos os dois verem branco, a roda que ja estava parada cobntinua parada
+# verificar sensores de parada
 
 # A = Sensor Esquerda
 # B = Sensor Direita
 # C = Motor Roda esquerda
 # D = Motor Roda DIreita
 # E = Motor Central
-# F = Nada
+# F = Sensor Parada
 
 PORTA_SENSOR_ESQ = port.A
 PORTA_SENSOR_DIR = port.B
+PORTA_SENSOR_PARADA = port.F
 MOTOR_RODA_ESQ = port.C
 MOTOR_RODA_DIR = port.D
 MOTOR_CENTRAL = port.E
+LINHA_CRUZ = 4
+TEMPO_ENTRE_CRUZ = 5000
 
 linha_esq = False
 linha_dir = False
+linha_final = False
+agora = time.ticks_ms()
+ult_tempo = time.ticks_ms()
+contador = 0
+contador_linha = 0
 
 LIMIAR = 30
 HISTERESE = 5
 
 def atualizarSensores():
     global linha_esq
-    global linha_dir 
+    global linha_dir
     SENSOR_ESQ = color_sensor.reflection(PORTA_SENSOR_ESQ)
     SENSOR_DIR = color_sensor.reflection(PORTA_SENSOR_DIR)
     print('se', SENSOR_ESQ, 'sd', SENSOR_DIR)
@@ -46,10 +57,23 @@ def atualizarSensores():
             print("Não Linha Direita", SENSOR_DIR)
             linha_dir = False
     else:
-       if SENSOR_DIR > (LIMIAR + HISTERESE) :
+        if SENSOR_DIR > (LIMIAR + HISTERESE) :
             print("linha Direita", SENSOR_DIR)
             linha_dir = True
     print(linha_esq, linha_dir)
+
+def sensorFinal():
+    global linha_final
+    SENSOR_FINAL = color_sensor.reflection(PORTA_SENSOR_PARADA)
+    print('sf', SENSOR_FINAL)
+    if linha_final:
+        if SENSOR_FINAL < (LIMIAR - HISTERESE - 5) :
+            print("Não linha parada", SENSOR_FINAL)
+            linha_final = False
+    else:
+        if SENSOR_FINAL > (LIMIAR + HISTERESE + 5) :
+            print("linha Parada", SENSOR_FINAL)
+            linha_final = True
 # def atualizarSensores(valor_sensor):
 #    if valor_sensor > (LIMIAR + HISTERESE):
 #        print("Linha", valor_sensor)
@@ -60,6 +84,8 @@ def atualizarSensores():
 #    else:
 #        sensor_linha = False
 #    return sensor_linha
+
+
 
 def tras(velocidade):
     motor.run(MOTOR_RODA_DIR, -velocidade)
@@ -86,11 +112,32 @@ def girarDireita(velocidade):
 async def main():
     global linha_dir
     global linha_esq
-    await light_matrix.write("!!!")
-    velocidade = 650
-    while True:
+    global contador_linha
+    global agora
+    global ult_tempo
+    global contador
+    await light_matrix.write("!")
+    velocidade = 300
+    while (contador < LINHA_CRUZ):
+        # Sensor de parada
+        agora = time.ticks_ms()
+        sensorFinal()
+        if linha_final: 
+            contador_linha = contador_linha + 1
+            print("viu linha")
+            if contador_linha > 5:
+                if((ult_tempo + TEMPO_ENTRE_CRUZ) < agora):
+                    contador = contador + 1
+                    await light_matrix.write(str(contador))
+                    ult_tempo = agora
+                    print("confirmou linha")
+                    print("numero de cruzamentos" , contador)
+        else:
+            contador_linha = 0
+            print("n era linha")
+
+        # Sensores de movimento
         atualizarSensores()
-        # sensor mais proximo
         if (not linha_dir) and (not linha_esq):
             frente(velocidade)
         elif linha_dir and (not linha_esq):
@@ -99,6 +146,18 @@ async def main():
             girarEsquerda(velocidade)
         else:
             frente
-        await runloop.sleep_ms(20)
+        
+    # while True:
+    #     atualizarSensores()
+    #     # sensor mais proximo
+    #     if (not linha_dir) and (not linha_esq):
+    #         frente(velocidade)
+    #     elif linha_dir and (not linha_esq):
+    #         girarDireita(velocidade)
+    #     elif (not linha_dir) and linha_esq:
+    #         girarEsquerda(velocidade)
+    #     else:
+    #         frente
+
 
 runloop.run(main())
